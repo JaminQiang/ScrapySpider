8527e80ea43cb3a52625639430e3a13dce2706d9
强晓明 <qxm96731@gmail.com>
2015-05-31 23:46:03 +0800

    Cooking Scrapy Document

--- tutorial/items.py
+++ tutorial/items.py
@@ -5,12 +5,12 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-from scrapy.item import Item, Field
+import scrapy
 
-class ScrapyspiderItem(Item):
+
+class DmozItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    url = Field()
-    forum = Field()
-    poster = Field()
-    content = Field()
+    title = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
